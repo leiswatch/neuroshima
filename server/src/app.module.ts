f0aba3657206b15a0988cc6dbb60afd291b91f27
leiswatch <mickowal.dev@gmail.com>
2021-06-17 19:59:41 +0200

    Implement typeorm with postgres

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    // Internal modules
-    UsersModule,
-
     // External modules
     GraphQLModule.forRoot({ autoSchemaFile: true }),
+    TypeOrmModule.forRoot(),
+
+    // Internal modules
+    UsersModule,
   ],
   controllers: [],
   providers: [],
