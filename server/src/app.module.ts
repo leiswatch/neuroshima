a8592e93e2ff7017946356f4f766d4020fe3239a
leiswatch <mickowal.dev@gmail.com>
2021-07-11 19:31:00 +0200

    Fix autogenerating graphql schema

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     // External modules
-    GraphQLModule.forRoot({ autoSchemaFile: true }),
+    GraphQLModule.forRoot({ autoSchemaFile: join(process.cwd(), 'src/graphql/schema.graphql') }),
     TypeOrmModule.forRoot({ autoLoadEntities: true }),
 
     // Internal modules
